c06a8091a270013d88d88aeb620f29949ba351fb
Royce Kumar <69853766+roycekumar@users.noreply.github.com>
2020-10-26 17:24:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function draw() {
 background('#6C91C2');
 
   document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
-textSize(difference+70);
+textSize(difference);
 fill('#FFE787');
 text('Royce', 50, 400);
 }
